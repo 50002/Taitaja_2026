e8219ef46afc40b5d46a5275db0aa760ef58b801
veeti-from <142967295+veeti-from@users.noreply.github.com>
2026-01-29 14:21:25 +0200

    adadad

--- speedrun-tower/Scenes/tutorial.gd
+++ speedrun-tower/Scenes/tutorial.gd
@@ -4,3 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameMusic.play()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
